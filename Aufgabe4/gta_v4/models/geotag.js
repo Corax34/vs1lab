15a4fa7499904ab0570e662a6f6270756b23162f
Niklas Arnitz <niklas.arnitz@icloud.com>
2022-06-15 13:13:13 +0200

    Add UUIDs to Geotag

--- Aufgabe4/gta_v4/models/geotag.js
+++ Aufgabe4/gta_v4/models/geotag.js
@@ -1,9 +1,6 @@
 // File origin: VS1LAB A3
 
-/**
- * This script is a template for exercise VS1lab/Aufgabe3
- * Complete all TODOs in the code documentation.
- */
+import {v4 as uuidv4 } from 'uuid'
 
 /** * 
  * A class representing geotags.
@@ -11,6 +8,7 @@
  */
 class GeoTag {
     constructor(name, latitude, longitude, hashtag) {
+        this.id = uuidv4()
         this.name = name
         this.latitude = latitude
         this.longitude = longitude
